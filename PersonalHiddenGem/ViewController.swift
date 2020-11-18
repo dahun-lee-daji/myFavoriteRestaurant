26fd092156338a9b6bb364f9aabd65d0a99217b0
dahun-lee-daji <ssss6596@gmail.com>
2020-11-18 18:07:16 +0900

    Navigation Bar, btn in bar, Map 추가
    Navigation Bar 내부에 "+" btn 추가
    Apple map 추가

--- PersonalHiddenGem/ViewController.swift
+++ PersonalHiddenGem/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var appleMap : MKMapView!
+    @IBOutlet var addAnnotationBtn : UIButton!
+    let locationManager : CLLocationManager! = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        getLocationAuthorization()
+        locationManager.startUpdatingLocation()
+        appleMap.showsUserLocation = true
+        
+    }
+    
+    @IBAction func addAnnotation(_ sender: UIButton){
+        print("btn clicked")
+        
     }
 
+    
+    func getLocationAuthorization() {
+        
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            // 위치 정보 권한 허용
+            print("위치 정보 권한 허용 됨")
+            
+        case .denied :
+            // 위치 정보 권한 거절.
+            print("위치 정보 권한 거절 됨")
+            locationManager.requestWhenInUseAuthorization()
+            
+        default:
+            // 위치 정보 허용 결정을 아직 내리지 않음. notDetermined, restricted
+            print("위치 정보 권한 요청")
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 
 }
 
